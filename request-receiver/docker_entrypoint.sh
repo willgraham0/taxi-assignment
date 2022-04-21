2924729941bcd8a52c2d93fac7c5834e3e82c5aa
will.graham <will.graham0@gmail.com>
2022-04-21 13:48:58 +0100

    chore: wait for broker to start

--- request-receiver/docker_entrypoint.sh
+++ request-receiver/docker_entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "Attempting to connect to kafka broker at ${BROKER_HOST}:${BROKER_PORT}"
+while nc -z "${BROKER_HOST}" "${BROKER_PORT}"; ret=$?; [ "${ret}" -ne 0 ]; do
+  echo "kafka broker is unavailable - sleeping..."
+  sleep 1
+done
+  echo "kafka broker connection established."
+
 echo "Starting 'request-receiver' service"
 flask run --host=0.0.0.0
 exit
